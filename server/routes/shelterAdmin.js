548a5e81f11fc3749d4dbdb053cd43d69c51883f
iampiyush2004 <117506198+iampiyush2004@users.noreply.github.com>
2024-11-18 20:46:02 +0530

    divided the code

--- server/routes/shelterAdmin.js
+++ server/routes/shelterAdmin.js
@@ -1,77 +1,18 @@
 const {Router} = require('express');
+
+const { viewApplications,
+        acceptApplication,
+        rejectApplication } = require('../controllers/applications.controller');
+
+
 const router = Router();
-const Applications = require('../models/adoptionForm.model')
 
-router.get('/applications' , async(req,res) => {
-  try{ 
 
-    const applications = await Applications.find().populate('user');
 
-    if (!applications.length) {
-        return res.status(404).json({ message: 'No applications found' });
-      }
+router.get('/applications' ,viewApplications)
 
-    res.status(200).json(applications);
+router.put('/applications/:appId', acceptApplication);
 
-} catch (error) {
-    console.error('Error fetching applications:', error);
-    res.status(500).json({ message: 'Internal Server Error' });
-}
-})
-router.put('/applications/:appId', async (req, res) => {
-    const applicationId = req.params.appId;
-
-    try {
-        const updatedApplication = await Applications.findByIdAndUpdate(
-            applicationId,
-            { isApproved: true },
-            { new: true } 
-        );
-
-        if (!updatedApplication) {
-            return res.status(404).json({
-                message: "Application not found.",
-            });
-        }
-
-        res.status(200).json({
-            message: "Application approved!",
-            application: updatedApplication,
-        });
-    } catch (error) {
-        console.error('Error approving application:', error);
-        res.status(500).json({
-            message: "Internal Server Error",
-        });
-    }
-});
-router.put('/applications/reject/:appId', async (req, res) => {
-    const applicationId = req.params.appId;
-
-    try {
-        
-        const updatedApplication = await Applications.findByIdAndUpdate(
-            applicationId,
-            { isApproved: false }, 
-            { new: true } 
-        );
-
-        if (!updatedApplication) {
-            return res.status(404).json({
-                message: "Application not found.",
-            });
-        }
-
-        res.status(200).json({
-            message: "Application has been rejected!",
-            application: updatedApplication,
-        });
-    } catch (error) {
-        console.error('Error rejecting application:', error);
-        res.status(500).json({
-            message: "Internal Server Error",
-        });
-    }
-});
+router.put('/applications/reject/:appId',rejectApplication);
 
 module.exports = router;
